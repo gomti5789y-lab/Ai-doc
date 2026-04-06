bc70be1b8c5da7fcb09236af8d1c0654fd5d0e52
gomti5789y-lab <gomti5789y@gmail.com>
2026-04-07 00:52:24 +0530

    Create Home component for main page

---
+++ ai-pro/app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>AI Pro 🚀</h1>
+      <p>Apna AI tool use karo</p>
+
+      <a href="/dashboard">
+        <button style={{ padding: "10px 20px" }}>
+          Start Now
+        </button>
+      </a>
+    </main>
+  );
+}
